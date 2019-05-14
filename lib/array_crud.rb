15327ddc5d4a7b4505c099e8966eeb9003275911
Bernadette Davis <bsd231@nyu.edu>
2019-05-14 20:23:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  #return array[1:]
+  array.shift(1)
+  return array
 end
 
 def retrieve_element_from_index(array, index_number)
