8d3248e1f58fc5f8b126cf0e4d1ac4d248126807
Bernadette Davis <bsd231@nyu.edu>
2019-05-14 20:22:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,8 +11,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift(element)
-end
+  array.shift(1)
+  return arrayend
 
 def remove_element_from_end_of_array(array)
   array.shift(1)
